11bed77937f998701956ae222c53ef54ca989e83
MasterBlyat <gymple.toulemon@gmail.com>
2026-02-02 14:54:53 +0100

    feat(global): project init (with husky) HIR-28

---
+++ .commitlintrc.mjs
@@ -0,0 +1,70 @@
+/** @type {import("@commitlint/types").UserConfig} */
+const Configuration = {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  plugins: [
+    {
+      rules: {
+        'hir-reference': (parsed) => {
+          const hirPattern = /HIR-\d+$/;
+          const hasHirReference = hirPattern.test(parsed.header);
+          return [
+            hasHirReference,
+            hasHirReference
+              ? ''
+              : 'Le commit doit se terminer par une référence HIR-<numéro> (ex: feat(homepage): add login HIR-123)',
+          ];
+        },
+      },
+    },
+  ],
+  rules: {
+    'hir-reference': [2, 'always'],
+    'body-empty': [2, 'always'],
+    'footer-empty': [2, 'always'],
+    'header-case': [0, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 200],
+    'header-trim': [2, 'always'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-delimiter-style': [2, 'always', ['/', '-']],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'global',
+        'front',
+        'files',
+        'domain',
+        'communication',
+        'auth',
+        'gateway',
+        'packages',
+        'models',
+        'config',
+        'ui',
+      ],
+    ],
+    'subject-case': [0, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'libs',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'refacto',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
+
+export default Configuration;
---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { libraryConfig } from '@repo/eslint-config/library';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: [
+      'apps/**',
+      'packages/**',
+      'dist/**',
+      'node_modules/**',
+      '*.config.mjs',
+      '.*rc.mjs',
+    ],
+  },
+  ...libraryConfig,
+  {
+    languageOptions: {
+      parser: tsParser,
+    },
+  },
+  {
+    rules: {
+      // add override for any (a metric ton of them, initial conversion)
+      '@typescript-eslint/no-explicit-any': 'off',
+      // we generally use this in isFunction, not via calling
+      '@typescript-eslint/unbound-method': 'off',
+    },
+  },
+];
