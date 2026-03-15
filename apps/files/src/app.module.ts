e79d6983c26d8b9578f7f0f9df4c327cc767109e
vava56754 <valent.peyratout@gmail.com>
2026-03-15 11:21:17 +0100

    fix(global): fix redis on microservice HIR-40

--- apps/files/src/app.module.ts
+++ apps/files/src/app.module.ts
@@ -1,30 +1,43 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FilesModule } from './files/files.module';
 import { FileEntity } from '@repo/entities';
+import { cache } from '@repo/redis-config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { FilesModule } from './files/files.module';
 
 @Module({
   controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
       envFilePath: '.env',
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (configService: ConfigService) => {
+        return cache.AUTO({
+          REDIS_URL: configService.get<string>('REDIS_URL'),
+        });
+      },
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get<number>('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
         entities: [FileEntity],
+        host: configService.get('POSTGRES_HOST'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        port: configService.get<number>('POSTGRES_PORT'),
         synchronize: configService.get('NODE_ENV') !== 'production',
+        type: 'postgres',
+        username: configService.get('POSTGRES_USER'),
       }),
     }),
     FilesModule,
